7360193f3f6bf69a91b863e5f4e3c6e9d2cf9577
Luke Ghenco <lukeghenco@gmail.com>
2017-06-23 16:23:55 -0400

    Update solution code for ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,13 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
- helpers do
+  helpers do
     def logged_in?
-      !!session[:user_id]
+      !!current_user
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
   end
