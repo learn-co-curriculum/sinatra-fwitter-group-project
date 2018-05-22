d5a6510fcc6b31819340cfe441714141a9a80972
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-05-22 13:08:51 -0400

    merge conflicts resolved in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 require 'pry'
 describe 'User' do
+=======
+require 'spec_helper'
+
+describe 'User' do 
+>>>>>>> merge conflicts resolved in user spec
   before do
     @user = User.create(:username => "test 123", :email => "test123@aol.com", :password => "test")
   end
+  
   it 'can slug the username' do
     expect(@user.slug).to eq("test-123")
   end
@@ -12,10 +19,15 @@ describe 'User' do
     expect(User.find_by_slug(slug).username).to eq("test 123")
   end
 
+<<<<<<< HEAD
   it 'has a secure password' do
 
     expect(@user.authenticate("dog")).to eq(false)
 
+=======
+  it 'has a secure password' do 
+    expect(@user.authenticate("dog")).to eq(false)
+>>>>>>> merge conflicts resolved in user spec
     expect(@user.authenticate("test")).to eq(@user)
   end
 end
