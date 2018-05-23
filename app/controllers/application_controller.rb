e7b1287e84df708bf7586f1b180b6655349a61a7
Jonathan Mines <minesja@gmail.com>
2018-05-23 11:30:10 -0400

    Took out solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,20 +9,8 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "fwitter_secret"
   end
 
-  get '/' do
-    erb :index
-  end
 
-  helpers do
 
-    def logged_in?
-      !!current_user
-    end
-
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
-
-  end
+  
 
 end
