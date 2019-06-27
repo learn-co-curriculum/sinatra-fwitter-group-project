700d5fc1ced51e4d593da1c038028785d37b318b
Kellye Greene <kellyecodes@gmail.com>
2019-06-27 17:46:55 -0400

    Fixes issue #36

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -321,7 +321,10 @@ describe ApplicationController do
         fill_in(:username, :with => "becky567")
         fill_in(:password, :with => "kittens")
         click_button 'submit'
-        visit "/tweets/#{tweet2.id}/edit"
+        visit "tweets/#{tweet2.id}"
+        click_button "Delete Tweet"
+        expect(page.status_code).to eq(200)
+        expect(Tweet.find_by(:content => "look at this tweet")).to be_instance_of(Tweet)
         expect(page.current_path).to include('/tweets')
       end
 
