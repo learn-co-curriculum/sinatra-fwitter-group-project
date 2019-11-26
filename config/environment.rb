705716609978aa533bb167babc6b4995135a040d
Rob Cobb <rwcobbjr@gmail.com>
2019-11-26 11:16:28 -0500

    update configuration for ar 6

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 
 require_all 'app'
